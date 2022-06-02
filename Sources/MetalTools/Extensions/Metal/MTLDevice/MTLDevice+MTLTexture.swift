ce454012814b3230beec7593b5dde1914adc22b4
eugene <eugenebokhan@icloud.com>
2022-06-02 23:40:47 +0100

    remove mtktextureloader

---
+++ Sources/MetalTools/Extensions/Metal/MTLDevice/MTLDevice+MTLTexture.swift
@@ -0,0 +1,85 @@
+import Metal
+
+public extension MTLDevice {
+    
+    func texture(from cgImage: CGImage, usage: MTLTextureUsage = []) throws -> MTLTexture {
+        
+        // AlphaFirst – the alpha channel is next to the red channel, argb and bgra are both alpha first formats.
+        // AlphaLast – the alpha channel is next to the blue channel, rgba and abgr are both alpha last formats.
+        // LittleEndian – blue comes before red, bgra and abgr are little endian formats.
+        // Little endian ordered pixels are BGR (BGRX, XBGR, BGRA, ABGR, BGR).
+        // BigEndian – red comes before blue, argb and rgba are big endian formats.
+        // Big endian ordered pixels are RGB (XRGB, RGBX, ARGB, RGBA, RGB).
+        
+        // Valid parameters for RGB color space model are:
+        // 16  bits per pixel, 5  bits per component, kCGImageAlphaNoneSkipFirst
+        // 32  bits per pixel, 8  bits per component, kCGImageAlphaNoneSkipFirst
+        // 32  bits per pixel, 8  bits per component, kCGImageAlphaNoneSkipLast
+        // 32  bits per pixel, 8  bits per component, kCGImageAlphaPremultipliedFirst
+        // 32  bits per pixel, 8  bits per component, kCGImageAlphaPremultipliedLast
+        // 32  bits per pixel, 10 bits per component, kCGImageAlphaNone|kCGImagePixelFormatRGBCIF10
+        // 64  bits per pixel, 16 bits per component, kCGImageAlphaPremultipliedLast
+        // 64  bits per pixel, 16 bits per component, kCGImageAlphaNoneSkipLast
+        // 64  bits per pixel, 16 bits per component, kCGImageAlphaPremultipliedLast|kCGBitmapFloatComponents
+        // 64  bits per pixel, 16 bits per component, kCGImageAlphaNoneSkipLast|kCGBitmapFloatComponents
+        // 128 bits per pixel, 32 bits per component, kCGImageAlphaPremultipliedLast|kCGBitmapFloatComponents
+        // 128 bits per pixel, 32 bits per component, kCGImageAlphaNoneSkipLast|kCGBitmapFloatComponents
+        
+        guard let colorSpace = cgImage.colorSpace
+        else { throw MetalError.MTLDeviceError.textureCreationFailed }
+        
+        let pixelFormat: MTLPixelFormat
+        var optionalCGContext: CGContext?
+        
+        if colorSpace.model == .monochrome {
+            pixelFormat = .r8Unorm
+            optionalCGContext = CGContext(
+                data: nil,
+                width: cgImage.width,
+                height: cgImage.height,
+                bitsPerComponent: 8,
+                bytesPerRow: cgImage.width,
+                space: CGColorSpace(name: CGColorSpace.linearGray)!,
+                bitmapInfo: CGImageAlphaInfo.alphaOnly.rawValue
+            )
+        } else if colorSpace.model == .rgb {
+            pixelFormat = .rgba8Unorm
+            optionalCGContext = CGContext(
+                data: nil,
+                width: cgImage.width,
+                height: cgImage.height,
+                bitsPerComponent: 8,
+                bytesPerRow: cgImage.width * 4,
+                space: CGColorSpace(name: CGColorSpace.genericRGBLinear)!,
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue
+            )
+        } else { throw MetalError.MTLDeviceError.textureCreationFailed }
+        
+        guard let cgContext = optionalCGContext
+        else { throw MetalError.MTLDeviceError.textureCreationFailed }
+        
+        cgContext.draw(cgImage, in: CGRect(origin: .zero, size: CGSize(width: cgImage.width, height: cgImage.height)))
+        
+        guard let cgContext = optionalCGContext,
+                let baseAddress = cgContext.data
+        else { throw MetalError.MTLDeviceError.textureCreationFailed }
+        
+        let texture = try self.texture(
+            width: cgImage.width,
+            height: cgImage.height,
+            pixelFormat: pixelFormat,
+            options: [.cpuCacheModeWriteCombined],
+            usage: usage
+        )
+        
+        texture.replace(
+            region: texture.region,
+            mipmapLevel: 0,
+            withBytes: baseAddress,
+            bytesPerRow: cgContext.bytesPerRow
+        )
+        
+        return texture
+    }
+    
+}
--- Sources/MetalTools/MTLContext.swift
+++ Sources/MetalTools/MTLContext.swift
@@ -7,9 +7,7 @@ public final class MTLContext {
 
     public let device: MTLDevice
     public let commandQueue: MTLCommandQueue
-
     private var libraryCache: [Bundle: MTLLibrary] = [:]
-    public private(set) lazy var textureLoader = MTKTextureLoader(device: self.device)
 
     // MARK: - Init
 
@@ -47,8 +45,7 @@ public final class MTLContext {
 
     public func library(for bundle: Bundle) throws -> MTLLibrary {
         if self.libraryCache[bundle] == nil {
-            self.libraryCache[bundle] = try self.device
-                                                .makeDefaultLibrary(bundle: bundle)
+            self.libraryCache[bundle] = try self.device.makeDefaultLibrary(bundle: bundle)
         }
         return self.libraryCache[bundle]!
     }
@@ -57,47 +54,8 @@ public final class MTLContext {
         self.libraryCache = [:]
     }
     
-    /// Creates a texture out of CGImage
-    ///
-    /// - Parameters:
-    ///   - image: Image to create a texture from
-    ///   - srgb:
-    ///   Optional boolean flag that indicated whether or not CGImage is in sRGB color space
-    ///   If user omits the flag, function will check image's color space and only assign true if it equals CGColorSpace.sRGB,
-    ///   in all other cases MetalKit with decide on it on it's own
-    ///   - usage: Usage parameter of texture
-    ///   - generateMipmaps: Boolean flag that indicated whether or not to generate mipmaps
-    public func texture(from image: CGImage,
-                        srgb: Bool? = nil,
-                        usage: MTLTextureUsage = [.shaderRead],
-                        generateMipmaps: Bool = false) throws -> MTLTexture {
-        var options: [MTKTextureLoader.Option: Any] = [
-            .textureUsage: NSNumber(value: usage.rawValue),
-            // You have to wrap everything inside NSNumber or it will be ignored
-            .generateMipmaps: NSNumber(value: generateMipmaps)
-        ]
-        
-        // This may look a bit messy but actually handles all cases:
-        // 1. We let user ignore or pass .SRGB option
-        // 2. If user ignored it we try to infer it from the image itself
-        // 3. If image doesn't contain colorspace info and flag wasn't provided by user
-        //    we simply don't pass anything and let MetalKit decide for us
-        var isSRGB = srgb
-        if
-            isSRGB == nil,
-            let colorSpace = image.colorSpace,
-            colorSpace.name == CGColorSpace.linearSRGB
-        {
-            isSRGB = true
-        }
-        
-        if let _isSRGB = isSRGB {
-            options[.SRGB] = NSNumber(value: _isSRGB)
-        }
-
-        return try self.textureLoader
-                       .newTexture(cgImage: image,
-                                   options: options)
+    public func texture(from cgImage: CGImage, usage: MTLTextureUsage = []) throws -> MTLTexture {
+        return try self.device.texture(from: cgImage, usage: usage)
     }
 
 }
